a4b15aae02a0e0780193b93b2b5d8f7b2554e80f
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-03-11 13:26:03 +0000

    add vercel json in server

--- server/configs/allowedOrigins.js
+++ server/configs/allowedOrigins.js
@@ -1,1 +1,1 @@
-export const allowedOrigins = ['http://localhost:5173','http://localhost:5174'];
+export const allowedOrigins = ['http://localhost:5173','http://localhost:5174','http://localhost:4173',"https://startup-directory-plum.vercel.app"];
